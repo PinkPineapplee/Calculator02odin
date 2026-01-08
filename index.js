d4f643548b83e76792e4c8ed0e1a531873839a9c
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2026-01-08 17:01:35 +0100

    Add calculator object

---
+++ index.js
@@ -0,0 +1,17 @@
+
+//create calculator object
+
+let Calculotor ={
+    addition(a,b){
+     return a + b;
+    },
+    subtraction(a,b){
+        return a - b ;
+    },
+    division(a,b){
+        return a /b ;
+    },
+    multiplication(a,b){
+        return a *b ;
+    }
+    }
