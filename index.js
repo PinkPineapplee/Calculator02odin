0e28a8439d63d6eb248fda51726e23a29d0029f7
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2026-01-09 13:17:23 +0100

    add percent and sign methods to cal obj

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const buttons = document.querySelectorAll(".buttons");
 const minus = document.querySelector("#subtract");
-const add = document.querySelector("#add");
+const add = document.querySelector("#plus");
 const multiply = document.querySelector("#multiply");
 const divide = document.querySelector("#divide");
 const equals = document.querySelector("#equals");
@@ -13,6 +13,7 @@ const sign = document.querySelector("#sign")
 //create calculator object
 
 let Calculotor ={
+
     addition(a,b){
      return a + b;
     },
@@ -20,11 +21,20 @@ let Calculotor ={
         return a - b ;
     },
     division(a,b){
-        return a /b ;
+        return a / b ;
     },
     multiplication(a,b){
-        return a *b ;
+        return a * b ;
+    },
+    percentage(num){
+        return num/100;
+    },
+    sign(num){
+     let changeSign = num
+     num == num ? changeSign= -num : changeSign= num;
+        return changeSign
     }
+
     }
 
     //funtion that performs calculation
