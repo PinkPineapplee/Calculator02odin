f4396183bca3325e251cae56fc37d31bbfc7ba9c
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2026-01-15 17:28:03 +0100

    Add calculator class

--- index.js
+++ index.js
@@ -12,31 +12,19 @@ const sign = document.querySelector("#sign")
 
 //create calculator object
 
-class Calculotor ={
-    constructor(){
-      addition(a,b){
-     return a + b;
-    },
-    subtraction(a,b){
-        return a - b ;
-    },
-    division(a,b){
-        return a / b ;
-    },
-    multiplication(a,b){
-        return a * b ;
-    },
-    percentage(num){
-        return num/100;
-    },
-    sign(num){
-     let changeSign = num
-     num == num ? changeSign= -num : changeSign= num;
-        return changeSign
-    }
-     
-
-    }
+class Calculation ={
+    constructor(firstNum, operator, secondNum){
+        this.firstNum = firstNum;
+        this.secondNum = firstNum;
+        this.operator = {
+        "-": (a, b) => a - b,
+        "+": (a, b) => a + b,
+        "/": (a, b) => a / b,
+        "*": (a, b) => a * b,
+        "**": (a, b) => a ** b,
+        "+/-": a => a === a? -a : a,
+        "%": a => a/100
+  };
     
     }
 
@@ -48,32 +36,8 @@ function Calculator() {
     "+": (a, b) => a + b
   };
 
-  this.calculate = function(str) {
+  let calc = new Calculation(firstNum, operator, secondNum)
 
-    let split = str.split(' '),
-      a = +split[0],
-      op = split[1],
-      b = +split[2];
-
-    if (!this.methods[op] || isNaN(a) || isNaN(b)) {
-      return NaN;
-    }
-
-    return this.methods[op](a, b);
-  };
-
-  this.addMethod = function(name, func) {
-    this.methods[name] = func;
-  };
-}
-
-let powerCalc = new Calculator;
-powerCalc.addMethod("*", (a, b) => a * b);
-powerCalc.addMethod("/", (a, b) => a / b);
-powerCalc.addMethod("**", (a, b) => a ** b);
-
-let result = powerCalc.calculate("2 ** 3");
-alert( result ); // 8
    function populateScreen(){
     
    } 
